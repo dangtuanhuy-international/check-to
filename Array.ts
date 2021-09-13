f9d460b28beb1eb4c248ca7136961a3d119cf404
HuyDang <huy.dang@bbv.vn>
2021-09-13 21:56:06 +0700

    'init'

---
+++ Array.ts
@@ -0,0 +1,167 @@
+[
+    {
+        id: "567",
+        name: "Test_01",
+        catalog: [
+            {
+                id: "Catalog_01",
+                name: "Cat01",
+                section: [
+                    {
+                        id: "sec_1",
+                        name: "sec_1",
+                        check: [
+                            {
+                                type: 1,
+                                id: "check_sec_1",
+                                name: "Ch1",
+                            },
+                            {
+                                type: 2,
+                                id: "check_sec_2",
+                                name: "Ch1",
+                            },
+                        ],
+                    },
+                    {
+                        id: "sec_2",
+                        name: "sec_2",
+                        check: [
+                            {
+                                type: 1,
+                                id: "check_sec_3",
+                                name: "Ch",
+                            },
+                            {
+                                type: 2,
+                                id: "check_sec_4",
+                                name: "Ch",
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                id: "Catalog_02",
+                name: "Cat02",
+                section: [
+                    {
+                        id: "sec_11",
+                        name: "sec_11",
+                        check: [
+                            {
+                                type: 1,
+                                id: "check_sec_11",
+                                name: "Ch11",
+                            },
+                            {
+                                type: 2,
+                                id: "check_sec_11",
+                                name: "Ch12",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+        check: [
+            {
+                type: 2,
+                id: "check001",
+                name: "Chili",
+            },
+            {
+                type: 2,
+                id: "check002",
+                name: "Lucy",
+            },
+            {
+                type: 1,
+                id: "check003",
+                name: "Kamar",
+            }
+        ],
+    },
+    {
+        id: "568",
+        name: "Test_01",
+        catalog: [
+            {
+                id: "Catalog_02",
+                name: "Cat01",
+                section: [
+                    {
+                        id: "sec_Catalog_02_1",
+                        name: "sec_1",
+                        check: [
+                            {
+                                type: 1,
+                                id: "sec_Catalog_check_1",
+                                name: "Ch",
+                            },
+                            {
+                                type: 2,
+                                id: "sec_Catalog_check_2",
+                                name: "Ch",
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                id: "Catalog_03",
+                name: "Catalog_03",
+                section: [
+                    {
+                        id: "sec_Catalog_03_1",
+                        name: "sec_1",
+                        check: [
+                            {
+                                type: 1,
+                                id: "sec_Catalog_check_3",
+                                name: "Ch1",
+                            },
+                            {
+                                type: 2,
+                                id: "sec_Catalog_check_4",
+                                name: "Ch1",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+        check: [
+            {
+                type: 2,
+                id: "check_1",
+                name: "Ch1",
+            },
+            {
+                type: 2,
+                id: "check_2",
+                name: "Ch1",
+            },
+            {
+                type: 1,
+                id: "check_3",
+                name: "Ch1",
+            },
+            {
+                type: 2,
+                id: "check_4",
+                name: "Ch1",
+            },
+            {
+                type: 1,
+                id: "check_5",
+                name: "Ch1",
+            },
+            {
+                type: 3,
+                id: "check_6",
+                name: "Ch1",
+            },
+        ],
+    },
+];
---
+++ NewArray.ts
@@ -0,0 +1,212 @@
+[
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 2,
+            id: "check001",
+            name: "Chili",
+        }
+    },
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 2,
+            id: "0002",
+            name: "Lucy",
+        }
+    },
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 1,
+            id: "00003",
+            name: "Kamar",
+        }
+    },
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: 'Catalog_01',
+        sectionId: 'sec_1',
+        check: {
+            type: 1,
+            id: "check_sec_1",
+            name: "Ch",
+        }
+    },
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: 'Catalog_01',
+        sectionId: 'sec_1',
+        check: {
+            type: 2,
+            id: "check_sec_2",
+            name: "Ch",
+        }
+    },
+    ,
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: 'Catalog_01',
+        sectionId: 'sec_2',
+        check: {
+            type: 1,
+            id: "check_sec_3",
+            name: "Ch",
+        }
+    },
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: 'Catalog_01',
+        sectionId: 'sec_2',
+        check: {
+            type: 2,
+            id: "check_sec_4",
+            name: "Ch",
+        }
+    },
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: 'Catalog_02',
+        sectionId: 'sec_11',
+        check: {
+            type: 1,
+            id: "check_sec_11",
+            name: "Ch11",
+        }
+    },
+    {
+        TestId: "567",
+        TestName: "Test_01",
+        Catalog_Id: 'Catalog_02',
+        sectionId: 'sec_11',
+        check: {
+            type: 2,
+            id: "check_sec_11",
+            name: "Ch12",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 2,
+            id: "check_1",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 2,
+            id: "check_2",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 2,
+            id: "check_3",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 2,
+            id: "check_4",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 2,
+            id: "check_5",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: null,
+        sectionId: null,
+        check: {
+            type: 2,
+            id: "check_6",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: 'Catalog_02',
+        sectionId: 'sec_Catalog_02_1',
+        check: {
+            type: 2,
+            id: "sec_Catalog_check_1",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: 'Catalog_02',
+        sectionId: 'sec_Catalog_02_1',
+        check: {
+            type: 2,
+            id: "sec_Catalog_check_2",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: 'Catalog_03',
+        sectionId: 'sec_Catalog_03_1',
+        check: {
+            type: 2,
+            id: "sec_Catalog_check_3",
+            name: "Ch1",
+        }
+    },
+    {
+        TestId: "568",
+        TestName: "Test_02",
+        Catalog_Id: 'Catalog_03',
+        sectionId: 'sec_Catalog_03_1',
+        check: {
+            type: 2,
+            id: "sec_Catalog_check_4",
+            name: "Ch1",
+        }
+    },
+]
